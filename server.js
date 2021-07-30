f290bea5f60db7cbcdd009e4fc8ef2c428f5e9e3
TharushaDilmith <tharushadilmith99@gmail.com>
2021-07-30 23:04:07 +0530

    Initialize project

---
+++ server.js
@@ -0,0 +1,35 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const monngoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+monngoose.connect(MONGODB_URI, {
+  useCreateIndex: true,
+  useFindAndModify: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}),
+  (error) => {
+    if (error) {
+      console.log("Database connecting error", error);
+    }
+  };
+
+monngoose.connection.once("open", () => {
+  console.log("Database connected");
+});
+
+app.listen(PORT, () => {
+  console.log("You are listening to port " + PORT);
+});
