60804109563b43259cb97aa0aa9a02a34e6ee739
Didula Lakminda <68744893+Didula-Lakminda@users.noreply.github.com>
2021-08-13 21:18:56 +0530

    Delete auth_src directory

--- auth_src/routes/Register_Routes/Admin_Register.route.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const { adminRegister } = require('../../controllers/Auth.controller');
-
-router.post('/register-admin', async(req, res) => {
-    await adminRegister(req.body, res);
-});
-
-module.exports = router;
