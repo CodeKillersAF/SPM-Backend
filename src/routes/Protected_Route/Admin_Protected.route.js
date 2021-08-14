7dd888fadbde4ddde39d1a40507922dde40e73ba
kawsikan <kawsinote@gmail.com>
2021-08-14 12:42:47 +0530

    auth route set to supplier

--- src/routes/Protected_Route/Admin_Protected.route.js
+++ src/routes/Protected_Route/Admin_Protected.route.js
@@ -1,8 +1,19 @@
 const router = require('express').Router();
 const { adminAuth } = require('../../controllers/Auth.controller');
+const {getAllSupplier, createSupplier} = require('../../controllers/supplier.controller');
+
+
 
 router.get('/admin-protected', adminAuth, async(req, res) => {
     return res.send("Welcome Admin");
+})
+
+router.get('/supplier', adminAuth, async(req, res) => {
+    await getAllSupplier(req,res);
+})
+
+router.post('/supplier', adminAuth, async(req, res) => {
+    await createSupplier(req,res);
 })
 
 module.exports = router;
