a02e25e704f96adfabcc71012cf50fea983c389d
kawsikan <kawsinote@gmail.com>
2021-08-14 20:35:35 +0530

    create & read route suppl item

--- src/routes/Protected_Route/Admin_Protected.route.js
+++ src/routes/Protected_Route/Admin_Protected.route.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { adminAuth } = require('../../controllers/Auth.controller');
 const { getAllSupplier, createSupplier, removeSupplier } = require('../../controllers/supplier.controller');
+const { getAllSupplyItem, createSupplyItem } = require('../../controllers/supplyItem.controller');
 
 
 
@@ -21,5 +22,16 @@ router.post('/supplier', adminAuth, async (req, res) => {
 router.delete('/supplier/:id', adminAuth, async (req, res) => {
     await removeSupplier(req, res);
 })
+
+// Author : Kawsikan Routes for supplier details
+// Get all supply item
+router.get('/supply-item', adminAuth, async (req, res) => {
+    await getAllSupplyItem(req, res);
+})
+// Add new supplier
+router.post('/supply-item', adminAuth, async (req, res) => {
+    await createSupplyItem(req, res);
+})
+
 
 module.exports = router;
