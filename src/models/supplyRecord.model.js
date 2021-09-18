ecee3dcc19a54cc0b69b46817b44ec8094ecbcf9
kawsikan <kawsinote@gmail.com>
2021-09-18 21:42:15 +0530

    added fields for model

--- src/models/supplyRecord.model.js
+++ src/models/supplyRecord.model.js
@@ -1,6 +1,35 @@
 const mongoose = require("mongoose");
 
 const SupplyRecordSchema = new mongoose.Schema({
+    supplier_name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    supply_item: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    qty: {
+        type: Number,
+        required: true,
+        trim: true
+    },
+    unit_price: {
+        type: Number,
+        required: true,
+        trim: true
+    },
+    total_price: {
+        type: Number,
+        required: true,
+        trim: true
+    }
 
 })
 
